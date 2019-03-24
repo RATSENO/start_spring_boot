6a367b1a2cbfd9917e4cfc57e43f5e2265f6c223
ratseno <onestar1014@gmail.com>
2019-03-24 17:33:08 +0900

    [ADD]Test 코드 작성
    - 테스트 클래스에 @WebMvcTest 어노테이션을 추가해서 특정 컨트롤러를 지정해 준다.
    @WebMvcTest어노테이션을 사용하면, @controller, @Component, @ControllerAdvice등이 작성된 코드를 인식할 수 있다.
    
    -컨트롤러를 테스트하려면 orgorg.springframework.test.web.servlet.MockMvc 타입의 객체를 사용해야 한다.
    @WebMvcTest와 같이 사용하면 별도의 생성 없이 주입(@AutoWired)만으로 코드를 작성할수 있기 때문에 편리하다.
    
    -MockMvc 객체의 경우 Perform()을 이용해 객체를 브라우저에서 서버의 URL을 호출하듯이 테스트를 진행할수 있고
    결과는 andExpect()를 이용해서 확일할 수 있다
    andExpect()는 결과 확인 외에도 Response에 대한 정보를 체크하는 용도로 사용할 수 있다.

--- boot01/src/test/java/org/zerock/Boot01ApplicationTests.java
+++ boot01/src/test/java/org/zerock/Boot01ApplicationTests.java
@@ -1,16 +1,27 @@
 package org.zerock;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.zerock.controller.SampleController;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(SampleController.class)
 public class Boot01ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
+	@Autowired
+	MockMvc mock;
+
+	@Test
+	public void testHello() throws Exception{
+		mock.perform(get("/hello")).andExpect(content().string("hello world"));
+	}
 }
